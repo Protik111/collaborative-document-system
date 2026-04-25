ffc21198ccfea480f462cdeedb6d142b49698517
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-04-25 20:01:54 +0600

    feat: all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { WorkspaceModule } from './workspace/workspace.module';
+import { DocumentModule } from './document/document.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, WorkspaceModule, DocumentModule, UserModule],
   controllers: [],
   providers: [],
 })

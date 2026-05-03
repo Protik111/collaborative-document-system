90142132b4867a1c715af5506018d7bb89f741ab
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-05-03 23:49:28 +0600

    feat: health route

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { WorkspaceModule } from './workspace/workspace.module';
 import { DocumentModule } from './document/document.module';
 import { UserModule } from './user/user.module';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [
@@ -20,5 +21,6 @@ import { UserModule } from './user/user.module';
     DocumentModule,
     UserModule,
   ],
+  controllers: [HealthController],
 })
 export class AppModule {}

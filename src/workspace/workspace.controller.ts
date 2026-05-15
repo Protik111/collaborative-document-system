82629836754d88537ab3c2d95d06905473d313da
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2026-05-15 20:50:02 +0600

    feat: workspace controllers

--- src/workspace/workspace.controller.ts
+++ src/workspace/workspace.controller.ts
@@ -1,7 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { WorkspaceService } from './workspace.service';
+import { CreateWorkspaceDto } from './dto/create-workspace.dto';
+import { Request } from 'express';
+import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
 
 @Controller('workspace')
 export class WorkspaceController {
   constructor(private readonly workspaceService: WorkspaceService) {}
+
+  @Post()
+  async createWorkspace(
+    @Body() createDto: CreateWorkspaceDto,
+    @Req() req: Request,
+  ) {
+    const user = req.user as { userId: string };
+    return this.workspaceService.create(createDto, user.userId);
+  }
+
+  @Get()
+  async findAll(@Req() req: Request) {
+    const user = req.user as { userId: string };
+    return this.workspaceService.findAllByUser(user.userId);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string, @Req() req: Request) {
+    const user = req.user as { userId: string };
+    return this.workspaceService.findByIdForUser(id, user.userId);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateDto: UpdateWorkspaceDto,
+    @Req() req: Request,
+  ) {
+    const user = req.user as { userId: string };
+    return this.workspaceService.update(id, updateDto, user.userId);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string, @Req() req: Request) {
+    const user = req.user as { userId: string };
+    return this.workspaceService.remove(id, user.userId);
+  }
 }
